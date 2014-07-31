92a96a302a7ff8ccb5b94cd63309ee0dc753d32c
Tetsuro Higuchi <tetsu@ageet.com>
2014-07-31 12:14:37 +0800

    [php] add php5.4 & mod_php

--- roles/webserver.rb
+++ roles/webserver.rb
@@ -2,8 +2,10 @@ name 'webserver'
 description "Systems that serve HTTP and HTTPS"
 run_list(
   "role[base]",
+  "recipe[php54-setup]",
   "recipe[apache-setup]",
-  "recipe[apache2::mod_ssl]"
+  "recipe[apache2::mod_ssl]",
+  "recipe[apache2::mod_php5]"
 )
 default_attributes(
   "apache" => {
