b5aee4d653ec7fc52ae564af380abefd7d6e7489
Tetsuro Higuchi <higuchi.tetsuro@gmail.com>
2014-09-12 10:17:24 +0800

    mysql-setup レシピを削除

--- site-cookbooks/mysql-setup/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default[:mysql][:server_root_password]   = 'root'
-default[:mysql][:server_repl_password]   = 'root'
-default[:mysql][:server_debian_password] = 'root'
-default[:mysql][:allow_remote_root] = true
-default[:database][:host] = 'localhost'
-default[:database][:database_names] = ['magcruise', 'test_magcruise'],
