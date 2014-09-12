be7043e4ebe8f7b33e3bb04ba2fbdbbee7e3ab89
Tetsuro Higuchi <higuchi.tetsuro@gmail.com>
2014-09-12 10:36:45 +0800

    composerのインストールを追加

--- site-cookbooks/apache-setup/recipes/default.rb
+++ site-cookbooks/apache-setup/recipes/default.rb
@@ -10,14 +10,26 @@ include_recipe 'apache2'
 include_recipe 'apache2::mod_rewrite'
 include_recipe 'apache2::mod_ssl'
 include_recipe 'apache2::mod_php5'
+include_recipe 'php-mcrypt'
+include_recipe 'composer'
 
 %w[
   php5-mysql
   php5-json
-  php5-mcrypt
 ].each do|pkg|
   package pkg do
     action :install
+  end
+end
+
+case node[:platform]
+when "ubuntu","debian"
+  link "#{node[:php][:ext_conf_dir]}/../mods-available/mcrypt.ini" do
+    to  "#{node[:php][:ext_conf_dir]}/mcrypt.ini"
+  end
+
+  execute 'enable mcrypt' do
+    command 'php5enmod mcrypt'
   end
 end
 
